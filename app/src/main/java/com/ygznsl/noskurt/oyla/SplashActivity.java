2a07e6b2423f7824da865bb59a0a6cf45c5c3b99
noskurt <emre15emre@hotmail.com>
2017-04-03 17:47:11 +0300

    Some changes

---
+++ app/src/main/java/com/ygznsl/noskurt/oyla/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.ygznsl.noskurt.oyla;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+//        try {
+//            Thread.sleep(1500);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        finish();
+    }
+}
